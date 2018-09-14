236e22b5c687b7319cde9f4c3522dda50b7dc57a
jsurville <julien.surville@gmail.com>
2018-09-14 12:12:47 +0200

    Création de la table Categories dans la BD avec update-database et création du controller de catégore CatogorieController

---
+++ Todolist/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Todolist.Data;
+using Todolist.Models;
+
+namespace Todolist.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        private TodoDbContext db = new TodoDbContext();
+        public IQueryable<Categorie> GetCategories()
+        {
+            return db.Categories;
+        }
+    }
+}
